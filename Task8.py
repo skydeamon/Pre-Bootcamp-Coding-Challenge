ff895ade0a42a4c2b659fc739c217d6cb5ba0151
skydeamon <skycharmingdeamon@gmail.com>
2020-11-07 12:45:28 +0200

    Handled singularity and pluralitr on Task 8

--- Task8.py
+++ Task8.py
@@ -9,4 +9,8 @@ Pre-bootcamp challenges tast 8
 """
 
 def convert_number_to_time(number):
-    return f"{int(number//60)} hours, {int(number%60)} minutes"
+    hr = int(number//60)
+    mins = int(number%60)
+    hour = 'hour' if(hr==1) else 'hours'
+    minute = 'minute' if(mins == 1) else 'minutes'
+    return f"{int(number//60)} {hour}, {int(number%60)} {minute}"
