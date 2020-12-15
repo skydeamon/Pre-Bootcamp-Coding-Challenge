6da3b7979b7a409f39f3ff49f481d64529edbc76
skydeamon <skycharmingdeamon@gmail.com>
2020-12-15 18:44:50 +0200

    Modified code

--- Task11.py
+++ Task11.py
@@ -9,9 +9,12 @@ Pre-bootcamp challenges tast 11
 
 # Retruns common letters between two strings
 def common(str1,str2):
-    comm = []
+    common = []
     for let1 in list(str1):
         for let2 in list(str2):
             if(let2==let1 and (let1 not in common)):
                 common.append(let1)
     return ",".join(common)
+
+if __name__ == '__main__':
+	print(common('Jade','Makwela'))
--- Task9.py
+++ Task9.py
@@ -9,8 +9,5 @@ Pre-bootcamp challenges tast 9
 
 # Sums multiples of 3 and 5
 def sum_multiples(number):
-    multiples = []
-    for i in range(number+1):
-        if(i%3==0 or i%5==0):
-            multiples.append(i)
+    multiples = [i for i in range(number+1) if(i%3==0 or i%5==0)]
     return sum(multiples)
